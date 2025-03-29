d14ebc678493c50d8c07b00330170f913f4b82ba
selidrissi <s.elidrissi1992@gmail.com>
2025-03-29 15:10:59 +0000

    create login page template

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatFormField, MatInput, MatInputModule} from '@angular/material/input';
+import {MatButton} from '@angular/material/button';
+import {RouterLink} from '@angular/router';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
+  standalone: true,
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatFormField,
+    MatInputModule,
+    MatInput,
+    MatFormField,
+    MatButton,
+    RouterLink
+  ]
 })
 export class LoginComponent {
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required)
+  });
 
+  get email() {
+    return this.loginForm.get('email');
+  }
+  get password() {
+    return this.loginForm.get('password');
+  }
 }
