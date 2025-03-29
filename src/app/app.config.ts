3f80a92307059afc3c56244b3b90f0c7080aff70
selidrissi <s.elidrissi1992@gmail.com>
2025-03-29 15:11:23 +0000

    add angular fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {getAuth, provideAuth} from '@angular/fire/auth';
+import {environment} from '../environment/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)]
-
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth())
+  ]
 };
---
+++ src/environment/environment.ts
@@ -0,0 +1,12 @@
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    projectId: "angular-sing-up-16e2b",
+    appId: "1:336240052039:web:4a3a7f42bfbd927d8f6e62",
+    storageBucket: "angular-sing-up-16e2b.firebasestorage.app",
+    apiKey: "AIzaSyAJLpYHsnq_Z3yUGP3TCnUnfz7COCF-s3g",
+    authDomain: "angular-sing-up-16e2b.firebaseapp.com",
+    messagingSenderId: "336240052039"
+  }
+}
