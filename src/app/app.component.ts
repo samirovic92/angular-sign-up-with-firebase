f9adbb501d907187d1675f7109417c3e361455b6
selidrissi <s.elidrissi1992@gmail.com>
2025-03-29 13:40:17 +0000

    create all necessary components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatIcon} from '@angular/material/icon';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true,
+  imports: [RouterOutlet, MatToolbar, MatIcon, MatButton, MatIcon, RouterLink]
 })
 export class AppComponent {
-  title = 'angular-sign-up-with-firebase';
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {LandingComponent} from './components/landing/landing.component';
+import {LoginComponent} from './components/login/login.component';
+import {HomeComponent} from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: LandingComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'sing-up',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+];
